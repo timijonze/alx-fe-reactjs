90976ec4e06ce40365d14cde75ef866e0e379602
timijonze <olutimehinalu@gmail.com>
2024-11-09 11:04:35 +0100

    add code to files

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main>
+        <main style={{fontSize: '30px', color: 'green'}}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
