a9d0be04cbf5e2fc694d55a2b1b884cb726ede1a
timijonze <olutimehinalu@gmail.com>
2024-11-09 10:22:58 +0100

    add content to alx-fe-reactjs file

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{fontSize: '30px', color: 'green'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
