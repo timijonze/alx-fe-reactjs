68e4a446014bfa4bc74048e35cccf4a24265e863
timijonze <olutimehinalu@gmail.com>
2024-11-09 14:51:39 +0100

    Updated App and UserProfile components with styling changes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,10 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/header'
+import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -47,6 +48,9 @@ function App() {
     </div>
     <div>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+    <div>
+      <Counter />
     </div>
     </>
   )
