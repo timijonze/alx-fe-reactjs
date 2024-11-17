3d9356fd8482f1a74d06d1717643cc6a56d18332
timijonze <olutimehinalu@gmail.com>
2024-11-17 15:07:10 +0100

    update app.jsx file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,12 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import { useRecipeStore } from './components/recipeStore'
+import RecipeDetails from './components/RecipeDetails'
+import EditRecipeForm from './components/EditRecipeForm'
+import SearchBar from './components/SearchBar'
+import FavoritesList from './components/FavoritesList'
+import RecommendationsList from './components/RecommendationsList'
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+
+
+const RecipeDetailsWrapper = (props) => 
+  { const location = useLocation(); 
+    const recipeId = location.state?.recipeId; 
+    return <RecipeDetails recipeId={recipeId} {...props} />; 
+  };
+
+
+  const EditRecipeFormWrapper = (props) => 
+    { const location = useLocation(); 
+      const recipeId = location.state?.recipeId; 
+      return <EditRecipeForm recipeId={recipeId} {...props} />; 
+    };
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +52,28 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <nav> 
+        <Link to="/">Home</Link> | <Link to="/favorites">Favorites</Link>
+      </nav>
+      <div> 
+        <AddRecipeForm /> 
+      </div> 
+      <div> 
+        <RecipeList /> 
+        <Routes> 
+          <Route path="/" element={<RecipeList />} /> 
+          <Route path="/add" element={<AddRecipeForm />} /> 
+          <Route path="/recipes/:id" element={<RecipeDetailsWrapper />} /> 
+          <Route path="/edit/:id" element={<EditRecipeFormWrapper />} /> 
+          </Routes>
+        </div>
+        </Router>
+        <div>
+          <SearchBar />
+        </div>
+        <div>
+          <RecommendationsList />
+        </div>
     </>
   )
 }
