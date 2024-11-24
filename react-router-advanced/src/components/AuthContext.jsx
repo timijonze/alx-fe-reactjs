202c2c28248dfd7b878c7f066a1df2832e6330b6
timijonze <olutimehinalu@gmail.com>
2024-11-24 15:25:26 +0100

    implement protected routes

---
+++ react-router-advanced/src/components/AuthContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export default AuthProvider;
+export { useAuth, AuthContext };
