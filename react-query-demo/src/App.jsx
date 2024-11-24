bc9aa0850edeb2718338af8729a70106abcf9648
timijonze <olutimehinalu@gmail.com>
2024-11-24 10:37:51 +0100

    define home component

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,9 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import PostsComponent from './components/PostsComponent'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+const Home = () => 
+  { return <h2>Home Page</h2>; 
+    
+  };
+
+const queryClient = new QueryClient ();
+
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -28,6 +40,22 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <nav>
+        <Link to="/">Home</Link>
+        {' | '}
+        <Link to="/posts">Posts</Link>
+      </nav>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/posts" element={<PostsComponent />}></Route>
+      </Routes>
+      </Router>
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
+
+      
     </>
   )
 }
