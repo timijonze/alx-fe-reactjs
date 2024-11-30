49ded9059fa059ad149a64fb1c896c37079d3ac7
timijonze <olutimehinalu@gmail.com>
2024-12-01 00:18:56 +0100

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -24,10 +24,10 @@ function UserProfile() {
         alt="User"
         className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
       />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">
         John Doe
       </h1>
-      <p className="text-gray-600 text-base sm:text-sm md:text-base">
+      <p className="text-gray-600 text-base sm:text-sm md:text-base hover:text-blue-500">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
