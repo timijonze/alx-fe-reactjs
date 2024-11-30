eac933078941ad6e7b6f2b6cb7eb57ff34521518
timijonze <olutimehinalu@gmail.com>
2024-12-01 00:16:33 +0100

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,23 @@
 function UserProfile() {
   return (
     <div
-      className="user-profile"
-      bg-gray-100
-      p-8
-      max-w-sm
-      mx-auto
-      my-20
-      rounded-lg
-      shadow-lg
-      sm:p-4
-      md:p-8
-      sm:max-w-xs
-      md:max-w-sm
+      className="user-profile
+        bg-gray-100
+        p-8
+        max-w-sm
+        mx-auto
+        my-20
+        rounded-lg
+        shadow-lg
+        sm:p-4
+        md:p-8
+        sm:max-w-xs
+        md:max-w-sm
+        hover:scale-110
+        transition-transform
+        duration-300
+        ease-in-out
+      "
     >
       <img
         src="https://via.placeholder.com/150"
