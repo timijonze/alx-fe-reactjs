888cf1e6833814d3d67381c3bf7ff6c80cb6f582
timijonze <olutimehinalu@gmail.com>
2024-12-07 17:42:20 +0100

    add color export

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', 
+  theme: {
+    extend: {},
+  },
+  colors: {
+    customGreen: '#bcdc2',
+  },
+  plugins: [],
+};
